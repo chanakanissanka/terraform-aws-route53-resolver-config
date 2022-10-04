5a2803dbbf356c49a4d8f05b60d97dcab8910d48
chanaka-source <chanaka.nissanka@sourcedgroup.com>
2022-10-04 13:02:42 +1300

    modular approach

---
+++ example/main.tf
@@ -0,0 +1,18 @@
+module "r53-resolver-enpoints-rules" {
+    source = "git::https://github.com/chanakanissanka/terraform-aws-route53-resolver-endpoints"
+    rules = [
+    {
+      rule_name   = "r53-rule-1"
+      domain_name = "onprem.co.nz."
+      ips         = ["10.200.0.4", "10.200.0.5"]
+
+    },
+    {
+      rule_name   = "r53-rule-2"
+      domain_name = "internal.azure.co.nz."
+      ips         = ["10.200.0.4", "10.200.0.5"]
+
+    }]
+    vpc_id = "vpc-072b83a0cddfae6ba"
+    security_group_id = "sg-0bcf6fabd2f3c2b68"
+}
--- variable.tf
+++ variable.tf
@@ -1,26 +1,10 @@
 variable "security_group_id" {
   default = "sg-0bcf6fabd2f3c2b68"
 }
-variable "vpc_id" {
-  default = "vpc-072b83a0cddfae6ba"
-
-}
+variable "vpc_id" {}
 
 variable "rules" {
-  default = [
-    {
-      rule_name   = "r53-rule-1"
-      domain_name = "bar.foo."
-      ips         = ["192.168.10.10", "192.168.10.11"]
-
-    },
-    {
-      rule_name   = "r53-rule-2"
-      domain_name = "example.com."
-      ips         = ["192.168.10.10", "192.168.10.11"]
-
-    }
-  ]
+  default = []
   type = list(any)
 
 }
